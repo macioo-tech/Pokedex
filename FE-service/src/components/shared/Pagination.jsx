8911ce79e03e5db062a153dc313ed9d9b69628ec
mac <orno.morsoft@gmail.com>
2025-03-24 00:19:51 +0100

    corrected pagination

--- FE-service/src/components/shared/Pagination.jsx
+++ FE-service/src/components/shared/Pagination.jsx
@@ -1,57 +1,32 @@
-import { Button, IconButton } from "@material-tailwind/react";
+import { Typography, IconButton } from "@material-tailwind/react";
 import { GrNext, GrPrevious } from "react-icons/gr";
 
 const Pagination = ({ limit, page, setPage }) => {
-  const pages = [...Array(limit).keys()];
+  const enablePrev = page !== 0;
+  const enableNext = page !== limit;
 
-  const getItemProps = (index) => ({
-    variant: page === index.item ? "filled" : "text",
-    color: "gray",
-    onClick: () => {
-        setPage(index.item)
-        console.log("index", index.item);
-        console.log(page);
-        
-        
-    },
-  });
-
-  const next = () => {
-    if (page === limit - 1) return;
-    setPage((prev) => prev+1);
-  };
-
-  const prev = () => {
-    if (page === 0) return;
-    setPage((prev) => prev-1);
-  };
-
-  console.log("page", page);
-  console.log("limit", limit);
-  
   return (
-    <div className="flex justify-center items-center gap-4 py-10">
-      <Button isPill variant="ghost" onClick={prev}>
-        <GrPrevious className="mr-1.5 h-4 w-4 stroke-2" />
-        Previous
-      </Button>
-      <div className="fle items-center gap-2 px-10">
-        {pages.map((item) => (
-          <IconButton
-            key={item}
-            {...getItemProps({ item })}
-            isCircular
-            variant="ghost"
-          >
-            {item + 1}
-          </IconButton>
-        ))}
-      </div>
-
-      <Button isPill variant="ghost" onClick={next}>
-        Next
-        <GrNext className="ml-1.5 h-4 w-4 stroke-2" />
-      </Button>
+    <div className="flex justify-center items-center gap-2">
+      {enablePrev ? (
+        <IconButton size="sm" color="secondary" variant="ghost" onClick={() => setPage((prev) => prev - 1)}>
+          <GrPrevious className="mr-1.5 h-4 w-4 stroke-2" />
+        </IconButton>
+      ) : null}
+      <Typography className="flex items-center gap-1 text-foreground">
+        Page
+        <Typography as="span" color="default" className="font-semibold">
+          {page + 1}
+        </Typography>
+        of
+        <Typography as="span" color="default" className="font-semibold">
+          {limit}
+        </Typography>
+      </Typography>
+      {enableNext ? (
+        <IconButton size="sm" color="secondary" variant="ghost" onClick={() => setPage((prev) => prev + 1)}>
+          <GrNext className="ml-1.5 h-4 w-4 stroke-2" />
+        </IconButton>
+      ) : null}
     </div>
   );
 };
